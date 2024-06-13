829eaefe90b407f21036e1c6d3899a75b95e9b03
federico <federicohrvatin@gmail.com>
2024-06-13 15:34:05 +0200

    login finale

--- SOSpeedy/src/view/Main.java
+++ SOSpeedy/src/view/Main.java
@@ -16,19 +16,24 @@ public class Main extends Application {
         // Creazione dell'interfaccia Home
         Admin admin = new Admin();
         Map<String,Scene> scenes = new HashMap<String,Scene>();
+        
         Login login = new Login(admin, scenes);
         scenes.put("login", new Scene(login.createContent()));
+        
         Home home = new Home(admin, scenes);
         scenes.put("home", new Scene(home.createContent()));
+        
         GestioneVisite gestioneVisite = new GestioneVisite(admin,scenes,primaryStage);
         scenes.put("gestioneVisite", new Scene(gestioneVisite.createContent()));
+        
         AggiungiVisita aggVisita = new AggiungiVisita(admin,scenes);
         scenes.put("aggiungiVisite", new Scene(aggVisita.createContent()));
+        
         GestioneTurno gestTurno = new GestioneTurno(admin,scenes,primaryStage);
         scenes.put("gestioneTurni", new Scene(gestTurno.createContent()));
 
 
-        primaryStage.setTitle("Admin");
+        primaryStage.setTitle("SoSpeedy");
         primaryStage.setScene(scenes.get("login"));
         primaryStage.show();
     }
